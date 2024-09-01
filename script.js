a0afce7d55ce13763cf1071abfda9a245c624ecc
CodingOrangutang0 <bernhard.oma11@gmail.com>
2024-09-01 16:03:54 +0200

    Create script.js
    copied directory of old account

---
+++ script.js
@@ -0,0 +1,10 @@
+
+
+//TODO Sette op knapp funksjonalitet
+console.log('before sent decleartion');
+function button_activated() {
+    document.getElementById("ideInput").value
+}
+console.log('after decleartion has been sent');
+document.getElementById("clickMe").onclick = button_activated();
+
