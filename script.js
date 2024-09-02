ad62c76a431f560332d9b59ebb32efb3ea9a3063
CodingOrangutang0 <bernhard.oma11@gmail.com>
2024-09-02 07:21:38 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,9 @@
 //TODO Sette op knapp funksjonalitet
 document.addEventListener('DomContentLoaded', function() {
   const textarea = document.querySelector('textarea');
-
+  if (textarea.value.trim() !== '') {
+    alert(textarea.value);
+  }
+});
   
 
