62c6e3e9424bd3f30332244e8b1be2083f745e47
CodingOrangutang0 <bernhard.oma11@gmail.com>
2024-09-03 16:17:31 +0200

    did js code AGAIN!

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-document.getElementById("buttoninput").addEventListener("click", function() {
-  let = document.getElementById("ideinput").value
+
+//TODO Sette op knapp funksjonalitet
+function save_as_file() {
+  const textarea = document.querySelector('textarea')
+
   if (textarea.value.trim() !== '') {
-    console.log("Sent")
-  };
-});
-
-
+    console.log("Idea Sent");
+  }
+}
+document.getElementById("buttoninput").addEventListener("click", save_as_file)
