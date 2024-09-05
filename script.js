5a64e5be2505349b6643415e24ff39edc571d70b
CodingOrangutang0 <bernhard.oma11@gmail.com>
2024-09-05 15:05:07 +0200

    chantged activeate variable

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 //TODO Sette op knapp funksjonalitet
-const activate = document.querySelector("ideainput1")
+const activate = document.getElementById("ideainput1")
 function save_as_file1() {
   const textarea = document.querySelector('textarea')
 
