57ed6a500e48fd804722e37d0cac7023b7e22ecf
CodingOrangutang0 <bernhard.oma11@gmail.com>
2024-09-15 14:35:52 +0200

    added explainatory comments

--- user.py
+++ user.py
@@ -2,6 +2,8 @@ import socket
 import requests
 import time
 import os
+def clear():# Clears the python terminal of visual junk.
+  os.system('cls' if os.name == 'nt' else 'clear')
 def command_mangaer():
   online = True
   while online:
@@ -12,4 +14,8 @@ def command_mangaer():
       print("These are the currently working commands")
       print("1. ?, You just used this commannd gongrats")
       print("2. clear:Should be used to clear the command line when things get a little cramped.")
+      print("3. getserver: Logs you on by sending a fetch request to the host server of the submission system network")
+    if currentcommand == "clear":
+      clear()
+
       
