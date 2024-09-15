c1f72ca9380226cbf7d67153e2fb4f044327ef0e
CodingOrangutang0 <bernhard.oma11@gmail.com>
2024-09-15 14:30:05 +0200

    Update user.py

--- user.py
+++ user.py
@@ -8,4 +8,8 @@ def command_mangaer():
     currentcommand = input("Press ? if your new:")
     if currentcommand == "":
       print("you seem not to have entered anything this could be an error")
+    if currentcommand == "?":
+      print("These are the currently working commands")
+      print("1. ?, You just used this commannd gongrats")
+      print("2. clear:Should be used to clear the command line when things get a little cramped.")
       
