4a53e3c8edc35aa94f0d288c43f69d3604a0c405
CodingOrangutang0 <bernhard.oma11@gmail.com>
2024-09-15 14:28:11 +0200

    made a simple command_manager framework

--- user.py
+++ user.py
@@ -1,1 +1,11 @@
-
+import socket 
+import requests
+import time
+import os
+def command_mangaer():
+  online = True
+  while online:
+    currentcommand = input("Press ? if your new:")
+    if currentcommand == "":
+      print("you seem not to have entered anything this could be an error")
+      
